e13ea48c98596d225fbad202997bcf424fc3f55a
Adriano Raiano <adriano@raiano.ch>
2021-05-03 16:22:06 +0200

    info about revalidate option

--- pages/index.js
+++ pages/index.js
@@ -43,6 +43,13 @@ export const getStaticProps = async ({ locale }) => ({
   props: {
     ...await serverSideTranslations(locale, ['common', 'footer']),
   },
+  // if using the approach with the live translation download, mening using i18next-locize-backend on server side,
+  // there is a reloadInterval for i18next-locize-backend that can be used to reload resources in a specific interval: https://github.com/locize/i18next-locize-backend#backend-options
+  // doing so it is suggested to also use the revalidate option, here:
+  // Next.js will attempt to re-generate the page:
+  // - When a request comes in
+  // - At most once every hour
+  // revalidate: 60 * 60, // in seconds
 })
 
 export default Homepage
